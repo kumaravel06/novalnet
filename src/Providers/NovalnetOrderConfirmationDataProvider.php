685ea4bd45937ace1c06ba5aa64dbc086a5f507c
kumaravel <58772719+kumaravel06@users.noreply.github.com>
2019-12-17 13:02:16 +0530

    Update NovalnetOrderConfirmationDataProvider.php

--- src/Providers/NovalnetOrderConfirmationDataProvider.php
+++ src/Providers/NovalnetOrderConfirmationDataProvider.php
@@ -49,6 +49,7 @@ class NovalnetOrderConfirmationDataProvider
 		$barzhlentoken = '';
 		$barzahlenurl = '';
 		$payments = $paymentRepositoryContract->getPaymentsByOrderId($order['id']);
+		$paymentHelper->printValues($payments);
 		if (!empty ($order['id'])) {
 			foreach($payments as $payment)
 			{
@@ -105,7 +106,7 @@ class NovalnetOrderConfirmationDataProvider
 			}
 					$comment .= (string) $comments;
 					$comment .= PHP_EOL;
-					$paymentHelper->printValues(json_decode($invoiceDetails, true));
+					
 		}	
 				  $payment_type = (string)$paymentHelper->getPaymentKeyByMop($payment->mopId);
 				  return $twig->render('Novalnet::NovalnetOrderHistory', ['comments' => html_entity_decode($comment),'barzahlentoken' => $barzhlentoken,'payment_type' => html_entity_decode($payment_type),'barzahlenurl' => $barzahlenurl]);
