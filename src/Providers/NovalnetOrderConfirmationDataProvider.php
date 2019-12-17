b9c5eecc2647243050ab9946a7c2195ebd4728fa
kumaravel <58772719+kumaravel06@users.noreply.github.com>
2019-12-17 12:10:55 +0530

    Update NovalnetOrderConfirmationDataProvider.php

--- src/Providers/NovalnetOrderConfirmationDataProvider.php
+++ src/Providers/NovalnetOrderConfirmationDataProvider.php
@@ -105,7 +105,7 @@ class NovalnetOrderConfirmationDataProvider
 			}
 					$comment .= (string) $comments;
 					$comment .= PHP_EOL;
-					$paymentHelper->printValues($comment);
+					$paymentHelper->printValues(var_export($db_details).PHP_EOL.$comment);
 		}	
 				  $payment_type = (string)$paymentHelper->getPaymentKeyByMop($payment->mopId);
 				  return $twig->render('Novalnet::NovalnetOrderHistory', ['comments' => html_entity_decode($comment),'barzahlentoken' => $barzhlentoken,'payment_type' => html_entity_decode($payment_type),'barzahlenurl' => $barzahlenurl]);
