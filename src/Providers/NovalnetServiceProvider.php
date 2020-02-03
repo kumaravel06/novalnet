d7778fcef3d074ac7de35acfded9de6e0cdd7da8
Kumaravel G <58772719+kumaravel06@users.noreply.github.com>
2020-02-03 17:00:26 +0530

    Update NovalnetServiceProvider.php

--- src/Providers/NovalnetServiceProvider.php
+++ src/Providers/NovalnetServiceProvider.php
@@ -428,7 +428,6 @@ class NovalnetServiceProvider extends ServiceProvider
 			    if ($event->getDocType() == Document::INVOICE) {
 					$event->addOrderPdfGeneration($orderPdfGenerationModel); 
 			    }
-			    $paymentHelper->printValues($comments);
 		} catch (\Exception $e) {
                     $this->getLogger(__METHOD__)->error('Adding PDF comment failed for order' . $order->id , $e);
         } 
