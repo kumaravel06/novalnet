3b71c507d9fa7341f5b062b682d386c830f441cc
Kumaravel G <58772719+kumaravel06@users.noreply.github.com>
2020-02-07 11:36:40 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -276,7 +276,8 @@ class PaymentService
      * @return string
      */
     public function getInvoicePrepaymentComments($requestData)
-    {     
+    {  
+	    $this->getLogger(__METHOD__)->error('comment',$requestData);
     $comments = '';
     $comments .= PHP_EOL . PHP_EOL . $this->paymentHelper->getTranslatedText('transfer_amount_text');
     $comments .= PHP_EOL . $this->paymentHelper->getTranslatedText('account_holder_novalnet') . $requestData['invoice_account_holder'];
